50ed540980c3a5c8a893775428c724cdd8f84f90
ma ☆ <114525589+kn1f3prty@users.noreply.github.com>
2024-09-18 07:41:49 -0300

    zsdzd

---
+++ perguntas.js
@@ -0,0 +1,106 @@
+criaCartao (
+    'Matemática',
+    'Quais os tipos de lados em um triângulo na geometria plana?',
+    'Equilátero, isósceles e escaleno.')
+    
+    criaCartao (
+    'Química',
+    'Qual a diferença de uma substância saturada para uma insaturada?',
+    'As substâncias saturadas possuem apenas ligações simples. Já as insaturadas ligações duplas ou triplas.')
+    
+    criaCartao (
+    'Matemática',
+    'Qual a fórmulada área de um triângulo equilátero?',
+    '\[
+    A = \frac{L^2 \sqrt{3}}{4}
+    \].')
+    
+    criaCartao (
+    'Português',
+    'Qual é a função do adjunto adverbial em uma frase?',
+    'O adjunto adverbial é um termo acessório que modifica o verbo, indicando circunstâncias como tempo, lugar, modo, intensidade, entre outros.')
+    
+    criaCartao (
+    'Matemática',
+    'Qual o teorema de Pitágoras?',
+    'a² = b² + c² com a sendo a hipotenusa.')
+    
+    criaCartao (
+    'Biologia',
+    'Qual é a principal função da mitose nas células eucarióticas?',
+    'A principal função da mitose é a divisão celular que resulta em duas células-filhas geneticamente idênticas à célula-mãe, permitindo o crescimento, a reparação e a regeneração dos tecidos.')
+    
+    criaCartao (
+    'Física',
+    'Qual é a fórmula para calcular a energia cinética de um objeto e qual é a unidade de medida?',
+    'A fórmula para calcular a energia cinética de um objeto é Ek=12mv2E_k = \frac{1}{2}mv^2Ek​=21​mv2, onde mmm é a massa do objeto e vvv é a sua velocidade. A unidade de medida é o Joule (J).')
+    
+    criaCartao (
+    'Biologia',
+    'Qual a porcentagem de água nos animais e nos vegetais?',
+    'Nos animais é de 75 à 85% e nos vegetais de 74%.')
+    
+    criaCartao (
+    'Biologia',
+    'Para o que servem as interações dipolo-dipolo que ocorrem entre moléculas de água?',
+    'Servem para estabilizá-las.')
+    
+    criaCartao (
+    'Biologia',
+    'O que a capilaridade representa?',
+    'A capacidade que a água possui de subir por estruturas porosas e tubos finos.')
+    
+    criaCartao(
+    'Biologia',
+    'Quais as principais vitaminas lipossolúveis?',
+    'As vitaminas A, D, E e K.')
+    
+    criaCartao(
+    'Biologia',
+    'Quais as principais vitaminas hidrossolúveis?',
+    'As do complexo B e vitamina C.')
+    
+    criaCartao(
+    'Biologia',
+    'A vitamina A também é conhecida como...?',
+    'Retinol.')
+    
+    criaCartao(
+    'Biologia',
+    'Qual a principal função da vitamina B2?',
+    'Participa do metabolismo das proteínas, lípidos e glicídos.')
+    
+    criaCartao(
+    'Biologia',
+    'Qual a ordem de classificação dos seres vivos na taxonomia?',
+    'Reficofage.')
+    
+    criaCartao (
+    'Português',
+    'Qual é a diferença entre a função denotativa e a função conotativa da linguagem?'
+    'A função denotativa foca na informação literal e objetiva. Já a função conotativa busca provocar emoções, sugestão ou uma interpretação mais subjetiva.')
+    
+    criaCartao (
+    'Química',
+    'Como o benzeno é classificado?'
+    'O benzeno é classificado como fechado, aromático, insaturado, normal e homogêneo.')
+    
+    criaCartao (
+    'História',
+    'Em que ano aconteceu a revolução francesa?',
+    A revolução ocorreu de 1789 a 1799.')
+    
+    criaCartao (
+    'História',
+    'Quando aconteceu a primeira guerra mundial?',
+    'A primeira guerra mundial começou em 1914 e perdurou até 1918.')
+    
+    criaCartao (
+    'História',
+    'Qual o estopim da primeira guerra mundial?',
+    'O estopim foi a morte do arquiduque Francisco Ferdinando da Áustria.')
+    
+    criaCartao (
+    'História',
+    'Quando começou a segunda guerra mundial e qual foi seu estopim?',
+    'A segunda guerra mundial teve início em 1939 e prosseguiu até 1945, seu estopim foi a invasão da Polônia.')
