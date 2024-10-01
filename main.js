479a1ebe611730f1cdb7a21f039a654536ebb2d2
ma ☆ <114525589+kn1f3prty@users.noreply.github.com>
2024-10-01 08:31:02 -0300

    tyt

--- main.js
+++ main.js
@@ -13,5 +13,11 @@ function criaCartao (categoria, pergunta, resposta){
   </div>
 </div>
    `
+   let respostaVisivel = false;
+   function viraCartao (){
+    respostaVisivel = !respostaVisivel;
+    cartao.classList.toggle('active', respostaVisivel);
+   }
+   cartao.addEventListener('click', viraCartao);
    container.appendChild(cartao);
 }
